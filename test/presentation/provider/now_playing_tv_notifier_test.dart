0f2614b59951650b52db601eb4cd5449ac1fbd8b
ianahmfac <me.business25@gmail.com>
2022-11-13 20:12:02 +0700

    Implement now playing test

---
+++ test/presentation/provider/now_playing_tv_notifier_test.dart
@@ -0,0 +1,69 @@
+import 'package:dartz/dartz.dart';
+import 'package:ditonton/common/failure.dart';
+import 'package:ditonton/common/state_enum.dart';
+import 'package:ditonton/domain/entities/tv_series.dart';
+import 'package:ditonton/domain/usecases/get_now_playing_tv_series.dart';
+import 'package:ditonton/presentation/provider/now_playing_tv_notifier.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'now_playing_tv_notifier_test.mocks.dart';
+
+@GenerateMocks([GetNowPlayingTvSeries])
+void main() {
+  late MockGetNowPlayingTvSeries mockGetNowPlayingTvSeries;
+  late NowPlayingTvNotifier notifier;
+  late int listenerCallCount;
+
+  setUp(() {
+    listenerCallCount = 0;
+    mockGetNowPlayingTvSeries = MockGetNowPlayingTvSeries();
+    notifier = NowPlayingTvNotifier(getNowPlayingTvSeries: mockGetNowPlayingTvSeries)
+      ..addListener(() {
+        listenerCallCount++;
+      });
+  });
+
+  final tTv = TvSeries(
+    backdropPath: 'backdropPath',
+    id: 1,
+    overview: 'overview',
+    posterPath: 'posterPath',
+    name: 'title',
+  );
+  final tTvList = <TvSeries>[tTv];
+
+  test('should change state to loading when usecase is called', () async {
+    // arrange
+    when(mockGetNowPlayingTvSeries.execute()).thenAnswer((_) async => Right(tTvList));
+    // act
+    notifier.fetchNowPlaying();
+    // assert
+    expect(notifier.state, RequestState.Loading);
+    expect(listenerCallCount, 1);
+  });
+
+  test('should change tv data when data is gotten successfully', () async {
+    // arrange
+    when(mockGetNowPlayingTvSeries.execute()).thenAnswer((_) async => Right(tTvList));
+    // act
+    await notifier.fetchNowPlaying();
+    // assert
+    expect(notifier.state, RequestState.Loaded);
+    expect(notifier.tvSeries, tTvList);
+    expect(listenerCallCount, 2);
+  });
+
+  test('should return error when data is unsuccessful', () async {
+    // arrange
+    when(mockGetNowPlayingTvSeries.execute())
+        .thenAnswer((_) async => Left(ServerFailure('Server Failure')));
+    // act
+    await notifier.fetchNowPlaying();
+    // assert
+    expect(notifier.state, RequestState.Error);
+    expect(notifier.message, 'Server Failure');
+    expect(listenerCallCount, 2);
+  });
+}
